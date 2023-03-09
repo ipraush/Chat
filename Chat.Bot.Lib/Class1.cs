40939127f8d3dbc2721ffe81f45a69d9fed5f75b
Zheka@RAUSHWEB <raushweb@yandex.ru>
2023-03-09 23:57:00 +0300

    Начальные пустые файлы

---
+++ Chat.Bot.Lib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Chat.Bot.Lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ Telegram.Chat.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Telegram.Chat.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
