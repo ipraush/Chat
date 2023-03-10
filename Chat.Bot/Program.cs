a27e43bf2117b90c2e0788dcf9c6bddd3c3ea604
Zheka@RAUSHWEB <raushweb@yandex.ru>
2023-03-10 13:59:12 +0300

    Реализовал бота чтобы он задавал случайные вопросы в команодной строке и выводил результат ответа на вопрос

--- Chat.Bot/Program.cs
+++ Chat.Bot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace Chat.Bot
 {
@@ -6,7 +8,42 @@ namespace Chat.Bot
     {
         static void Main(string[] args)
         {
+            // Подключаю фойл data.txt
+            // Необходимо в свойствах выбрать параметр "Копировать в выходной коталог" и установить занчение "Всегда копировать"
+            // Необхожимо указать кодировку файла для правильного отображения кирилических символов
+            // Выбираю "Файл" -> "Сохранить как" -> "Юнокод (UTF-8 с сигнатурой), кодовая страница 65001"
+            var lines = File.ReadAllLines("data.txt");
+            // Разбиваю строку на части 
+            var questions = lines
+                // в данном примере Split('|') выступает в качестве разделителя
+                // т.е. до | вопрос, после ответ
+                .Select(line => line.Split('|'))
+                .Select(line => (line[0], line[1]))
+                .ToList();
+            // подключаю метод Random
+            Random random = new();
+            var count = questions.Count;
+            while (true)
+            {
+                // эти 2 строчки изменят порядок строк на случайный
+                // т.е. вопросы будут задаваться не по порядку
+                var index = random.Next(count - 1);
+                var question = questions[index];
+                Console.WriteLine(question.Item1);
+                var tryAnswer = Console.ReadLine();
+                
+                if (tryAnswer == question.Item2)
+                {
+                    Console.WriteLine("Правильно!");
+                }
+                else
+                {
+                    Console.WriteLine("Не правильно!");
+                }
+            }
             Console.WriteLine("Hello World!");
+
+            // 01:11:51
         }
     }
 }
